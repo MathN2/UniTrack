cfc2bd20c181f7a7067a82955f08cf80b072f7ad
MathN2 <matheus-novais-de-ramos@hotmail.com>
2026-09-06 01:03:52 -0300

    feat: Contruindo classe Campus

--- src/main/java/br/com/unitrack/model/Campus.java
+++ src/main/java/br/com/unitrack/model/Campus.java
@@ -1,5 +1,29 @@
 package br.com.unitrack.model;
 
+import br.com.unitrack.core.ArvoreBinaria;
+
 public class Campus {
-    
+    private String nome;
+    private ArvoreBinaria arvoreAlunos;
+
+    public Campus(String nome) {
+        this.nome = nome;
+        this.arvoreAlunos = new ArvoreBinaria();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void adicionarAluno(Aluno aluno) {
+        arvoreAlunos.inserirAluno(aluno);
+    }
+
+    public Aluno buscarAluno(String matricula) {
+        return arvoreAlunos.buscarPorMatricula(matricula);
+    }
+
+    public Aluno buscarAlunoPorNome(String nome) {
+        return arvoreAlunos.buscarPorNome(nome);
+    }
 }
